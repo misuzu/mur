a2ae2a1e922e4df83701f8d85f6b4a32d0246acb
misuzu <bakalolka@gmail.com>
2025-10-15 01:05:49 +0300

    home-assistant-operating-system: fix meta.maintainers & passthru.updateScript

--- pkgs/home-assistant-operating-system/update.sh
+++ pkgs/home-assistant-operating-system/update.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p curl jq
 
-cd "$(dirname "${BASH_SOURCE[0]}")"
+cd pkgs/$UPDATE_NIX_PNAME
 
 curl https://api.github.com/repos/home-assistant/operating-system/releases/latest | jq -r '
 {
   "aarch64-linux": ("haos_generic-aarch64-" + .tag_name + ".qcow2.xz" as $name | first(.assets[] | select(.name == $name)) | {
     url: .browser_download_url,
-    hash: .digest
+    sha256: .digest | sub("^sha256:"; "")
   }),
   "x86_64-linux": ("haos_ova-" + .tag_name + ".qcow2.xz" as $name | first(.assets[] | select(.name == $name)) | {
     url: .browser_download_url,
-    hash: .digest
+    sha256: .digest | sub("^sha256:"; "")
   }),
   version: .tag_name
 }
