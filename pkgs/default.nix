878419439fefc9cb7ee180688e91b6544006e3ac
misuzu <bakalolka@gmail.com>
2025-10-18 11:17:15 +0300

    Use lib.filesystem.packagesFromDirectoryRecursive for packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,12 +1,5 @@
 { pkgs }:
-let
-  path = ./.;
-in
-with pkgs.lib;
-mapAttrs (n: v: pkgs.callPackage v { }) (
-  filterAttrs (n: v: builtins.pathExists v) (
-    mapAttrs (n: v: path + "/${n}/package.nix") (
-      filterAttrs (n: v: v == "directory") (builtins.readDir path)
-    )
-  )
-)
+pkgs.lib.filesystem.packagesFromDirectoryRecursive {
+  inherit (pkgs) callPackage;
+  directory = ./by-name;
+}
