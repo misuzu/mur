88f9e3b0bd6cb66acb09be8f919d2616f235201a
misuzu <bakalolka@gmail.com>
2025-10-18 12:11:05 +0300

    lib.filesystem.packagesFromDirectoryRecursive is not that great, use custom solution

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,14 @@
 { pkgs }:
-pkgs.lib.filesystem.packagesFromDirectoryRecursive {
-  inherit (pkgs) callPackage;
-  directory = ./by-name;
-}
+let
+  inherit (pkgs) lib;
+  path = ./by-name;
+  scope = lib.mapAttrs (n: v: callPackage v { }) (
+    lib.filterAttrs (n: v: builtins.pathExists v) (
+      lib.mapAttrs (n: v: path + "/${n}/package.nix") (
+        lib.filterAttrs (n: v: v == "directory") (builtins.readDir path)
+      )
+    )
+  );
+  callPackage = pkgs.newScope scope;
+in
+scope
